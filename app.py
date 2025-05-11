89a97d110b883750b5806ba364dd78314decec73
Iudex_nuevilette <133047718+Haritachi@users.noreply.github.com>
2025-05-11 17:57:31 +0530

    app.py

--- app.py
+++ app.py
@@ -338,5 +338,7 @@ def predict():
 
 # ------------------- Main Entry -------------------
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000)) 
     app.run(host='0.0.0.0', port=port, debug=False)
